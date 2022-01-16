bb4669822971c789d1aa6594c05329649a6bca93
Michael Bowen <michaelbowen1092@gmail.com>
2022-01-15 21:37:16 -0500

    commit tree 6fe1cca84175f0032b896ecf837ebb35a2e6cc56

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function(toDo = "go to the office"){
+    return `This Monday, I will ${toDo}.`
+}
+function wrapAdjective(flair = "*"){
+    const nice = function(nice = "a hard worker"){
+        return `You are ${flair}${nice}${flair}!` 
+    }
+    return nice
+    
+}
